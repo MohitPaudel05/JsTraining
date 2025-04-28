5fc2fb119790a9152afeaf483ff6613392bb1196
MohitPaudel05 <mohitpoudel39@gmail.com>
2025-04-28 21:50:14 +0545

    colorChangingEvent

--- project/colorChanger.js
+++ project/colorChanger.js
@@ -24,23 +24,48 @@ console.log(buttons)
 // })
 
 
-buttons.forEach((button)=> {
-   button.addEventListener('click',(e)=> {
-      if (e.target.id === "blue") {
-         body.style.backgroundColor = e.target.id;
-      }
-      if (e.target.id === "yellow") {
-         body.style.backgroundColor = e.target.id;
-      }
-      if (e.target.id === "red") {
-         body.style.backgroundColor = e.target.id;
-      }
-      if (e.target.id === "green") {
-         body.style.backgroundColor = e.target.id;
-      }
-      if (e.target.id === "white") {
-         body.style.backgroundColor = e.target.id;
-      }
-   })
+// buttons.forEach((button)=> {
+//    button.addEventListener('click',(e)=> {
+//       if (e.target.id === "blue") {
+//          body.style.backgroundColor = e.target.id;
+//       }
+//       if (e.target.id === "yellow") {
+//          body.style.backgroundColor = e.target.id;
+//       }
+//       if (e.target.id === "red") {
+//          body.style.backgroundColor = e.target.id;
+//       }
+//       if (e.target.id === "green") {
+//          body.style.backgroundColor = e.target.id;
+//       }
+//       if (e.target.id === "white") {
+//          body.style.backgroundColor = e.target.id;
+//       }
+//    })
 
-})
+// })
+
+//using switch case
+buttons.forEach((button) => {
+   button.addEventListener('click', (e) => {
+      switch (e.target.id) {
+         case "blue":
+            body.style.backgroundColor = "blue";
+            break;
+         case "yellow":
+            body.style.backgroundColor = "yellow";
+            break;
+         case "red":
+            body.style.backgroundColor = "red";
+            break;
+         case "green":
+            body.style.backgroundColor = "green";
+            break;
+         case "white":
+            body.style.backgroundColor = "white";
+            break;
+         default:
+            console.log("Unknown color");
+      }
+   });
+});
